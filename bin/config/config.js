be3b576d526add0b16a42d5b50ba073ad4067f78
yunwisdom <qq695926920@163.com>
2021-04-25 16:00:10 +0800

    init

--- bin/config/config.js
+++ bin/config/config.js
@@ -74,6 +74,23 @@ module.exports = () => {
         useCpuCores: '0', // use number of CPU cores (using cluster) / 1 by default "-c, --useCpuCores <n>"
     };
 
+    config.slaves = {
+        host: '172.18.254.95', // hostname of database / localhost by default "-h, --host <n>"
+        port: '39090', // port number for mysql / 3306 by default "-o, --port <n>"
+        user: 'zhaoziyun', // username of database / root by default "-u, --user <n>"
+        password: 'ziyequma', // password of database / empty by default "-p, --password <n>"
+        database: 'xdata_slave', // database schema name "-d, --database <n>"
+        ipAddress: '0.0.0.0', // IP interface of your server / localhost by default "-r, --ipAddress <n>"
+        portNumber: '3000', // port number for app / 3000 by default "-n, --portNumber <n>"
+        ignoreTables: '', // comma separated table names to ignore "-i, --ignoreTables <n>"
+        socketPath: '', // unix socket path / not used by default "-S, --socketPath <n>"
+        storageFolder: '', // storage folder / current working dir by default / available only with local "-s, --storageFolder <n>"
+        apiPrefix: '', // api url prefix / "/api/" by default "-a, --apiPrefix <n>"
+        debug: false, // 是否开启调试模式
+        readOnly: false, // readonly apis / false by default "-y, --readOnly"
+        useCpuCores: '0', // use number of CPU cores (using cluster) / 1 by default "-c, --useCpuCores <n>"
+    };
+
     return {
         ...config,
     };
