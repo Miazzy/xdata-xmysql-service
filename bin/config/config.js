0623e8e74bb2fc4084db10b1cf80615b347bccd2
yunwisdom <qq695926920@163.com>
2021-05-07 10:56:05 +0800

    init

--- bin/config/config.js
+++ bin/config/config.js
@@ -89,7 +89,7 @@ module.exports = () => {
     };
 
     config.memorycache = {
-        version: 'v2.0.3',
+        version: 'v2.0.5',
         cacheddl: {
             'bs_seal_regist': `CREATE TABLE IF NOT EXISTS bs_seal_regist
             (
