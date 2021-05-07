b6158be7fdb2d548c06eead20791ba7208ee6c2c
yunwisdom <qq695926920@163.com>
2021-05-07 16:36:38 +0800

    init

--- bin/config/config.js
+++ bin/config/config.js
@@ -89,7 +89,7 @@ module.exports = () => {
     };
 
     config.memorycache = {
-        version: 'v2.2.1',
+        version: 'v2.2.2',
         cacheddl: {
             'bs_seal_regist': ``,
             'bs_goods_receive': ``,
