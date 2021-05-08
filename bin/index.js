c623842a7210598f268dfcd3a350c65d38f04e07
yunwisdom <qq695926920@163.com>
2021-05-08 11:37:29 +0800

    init

--- bin/index.js
+++ bin/index.js
@@ -24,6 +24,7 @@ const os = require('os');
 const config = require('./config/config');
 const tools = require('../lib/tools/tools').tools;
 const cache = require('../lib/cache/cache');
+const lock = require('../lib/lock/redisLock');
 const config = require("./config/config");
 const sqlitePath = `${process.cwd()}/` + config().service.dblitepath;
 const sqliteFile = `${process.cwd()}/` + config().service.sqlitepath;
@@ -61,39 +62,38 @@ const initSqliteDB = async(pool = { query: () => {} }, metaDB = {}) => {
     const init_wait_milisecond = config().memorycache.init_wait_milisecond;
     const keys = Object.keys(cacheddl);
     console.log(`cache ddl #init# >>>>>>>>>>>>>> `);
-
-    //TODO 开启分布式锁 https://github.com/xdata-database-repository/redislock
-
-    (async() => {
-        for await (tableName of keys) {
-            const cacheKey = `init_sqlite_${tableName}_${ipaddress}_${version}`;
-            const flag = await cache.getValue(cacheKey);
-            let initSQL = cacheddl[tableName];
-            try {
-                if (tools.isNull(initSQL) || initSQL == 'generate' || initSQL == 'auto') {
-                    initSQL = await generateDDL(database, tableName, pool);
+    //开启分布式锁
+    lock.lockExec('app:init_sqlite_db:lock', () => {
+        (async() => {
+            for await (tableName of keys) {
+                const cacheKey = `init_sqlite_${tableName}_${ipaddress}_${version}`;
+                const flag = await cache.getValue(cacheKey);
+                let initSQL = cacheddl[tableName];
+                try {
+                    if (tools.isNull(initSQL) || initSQL == 'generate' || initSQL == 'auto') {
+                        initSQL = await generateDDL(database, tableName, pool);
+                    }
+                } catch (error) {
+                    console.error(`generate ddl error:`, error);
                 }
-            } catch (error) {
-                console.error(`generate ddl error:`, error);
-            }
-            try {
-                if (flag != `true` && !tools.isNull(initSQL)) { // await sqliteDB.query(initSQL); // memoryDB.query(initSQL);
-                    sqliteDB.query(initSQL);
-                    sqlite3DB.exec('BEGIN TRANSACTION');
-                    sqlite3DB.exec(initSQL);
-                    sqlite3DB.exec('COMMIT');
-                    cache.setValue(cacheKey, `true`, 3600 * 24 * 365 * 1000);
-                    console.log(`cache key: ${cacheKey} flag: ${flag} init sql:`, initSQL);
-                    console.info(`cache key: ${cacheKey} flag: ${flag} init sql:`, initSQL);
-                    console.error(`cache key: ${cacheKey} flag: ${flag} init sql:`, initSQL);
+                try {
+                    if (flag != `true` && !tools.isNull(initSQL)) { // await sqliteDB.query(initSQL); // memoryDB.query(initSQL);
+                        sqliteDB.query(initSQL);
+                        sqlite3DB.exec('BEGIN TRANSACTION');
+                        sqlite3DB.exec(initSQL);
+                        sqlite3DB.exec('COMMIT');
+                        cache.setValue(cacheKey, `true`, 3600 * 24 * 365 * 1000);
+                        console.log(`cache key: ${cacheKey} flag: ${flag} init sql:`, initSQL);
+                        console.info(`cache key: ${cacheKey} flag: ${flag} init sql:`, initSQL);
+                        console.error(`cache key: ${cacheKey} flag: ${flag} init sql:`, initSQL);
+                    }
+                } catch (error) {
+                    console.error(`exec ddl error:`, error);
                 }
-            } catch (error) {
-                console.error(`exec ddl error:`, error);
+                await tools.sleep(init_wait_milisecond);
             }
-            await tools.sleep(init_wait_milisecond);
-        }
-    })();
-
+        })();
+    });
 }
 
 /**
@@ -116,76 +116,77 @@ const syncSqliteDB = async(pool = { query: () => {} }, metaDB = {}) => {
 
     console.log(`cache ddl #sync# start >>>>>>>>>>>>>> : ......`, `cache ddl #sync# keys >>>>>>>>>>>>>> :`, keys);
 
-    //TODO 开启分布式锁 https://github.com/xdata-database-repository/redislock
+    //TODO 开启分布式锁 
+    lock.lockExec('app:sync_sqlite_db:lock', () => {
+        (async() => { //拉取数据库数据
+            for await (tableName of keys) { // 根据配置参数选择，增量查询或者全量查询
 
-    (async() => { //拉取数据库数据
-        for await (tableName of keys) { // 根据配置参数选择，增量查询或者全量查询
+                const cacheKey = `sync_sqlite_${tableName}_${ipaddress}_${version}`;
+                const flag = await cache.getValue(cacheKey);
+                console.log(`cache key: ${cacheKey} flag: ${flag} . `);
+                if (flag == `true`) { /***************** 方案一 增量 *****************/
+                    try {
+                        //查询本地sqlite数据，获取当前最大值 id , xid
 
-            const cacheKey = `sync_sqlite_${tableName}_${ipaddress}_${version}`;
-            const flag = await cache.getValue(cacheKey);
-            console.log(`cache key: ${cacheKey} flag: ${flag} . `);
-            if (flag == `true`) { /***************** 方案一 增量 *****************/
-                try {
-                    //查询本地sqlite数据，获取当前最大值 id , xid
+                        //查询主数据库数据库大于当前最大值的数据 id 新增 //将多的数据同步新增过来
 
-                    //查询主数据库数据库大于当前最大值的数据 id 新增 //将多的数据同步新增过来
+                        //查询主数据库数据等于当前最大值得数据 xid 更新 //将多的数据同步更新过来
 
-                    //查询主数据库数据等于当前最大值得数据 xid 更新 //将多的数据同步更新过来
-
-                    //对小于等于当前最大值的数据，进行检查并更新操作，异步
-                } catch (error) {
-                    console.log(`increment sync error:`, error);
-                }
-            } else { /***************** 方案二 全量 *****************/
-                cache.setValue(cacheKey, `true`, 3600 * 24 * 365 * 1000);
-                const querySQL = `select * from ${tableName} order by id desc `; //需要检查ID是否存在
-                const qTableName = `${tableName}`;
-                console.log(`exec #sync# tablename#${tableName}# >>>>>>>>>>>>>> :`, keys, ` select sql :`, querySQL);
-                try {
-                    //查询主数据库所有数据，全部插入本地数据库中
-                    pool.query(querySQL, [], (error, rows, _fields) => {
-                        try {
-                            if (error) { //如果执行错误，则直接返回
-                                return console.log("mysql sync to sqlite >>>>> ", error);
-                            }
-                            (async() => {
-                                console.log(`database> querySQL: ${querySQL} tablename:`, qTableName, ' rows length:', rows.length);
-                                const pageSize = 1; // 10条批量执行
-                                let page = 1,
-                                    maxRow = 0,
-                                    maxPage = Math.ceil(rows.length / pageSize);
-
-                                sqlite3DB.exec('BEGIN TRANSACTION');
-                                while (page <= maxPage) {
-                                    try {
-                                        startPage = pageSize * (page - 1);
-                                        maxRow = pageSize * (page - 0);
-                                        const curRows = rows.slice(startPage, maxRow);
-                                        const statement = tools.parseInsertStatement(qTableName, curRows, metaDB);
-                                        let execstr = sqlstring.format(statement.query, statement.params);
-                                        execstr = execstr.replace(/\r|\n/g, ''); //执行插入语句前，先查询数据库中是否存在此数据，若存在，则不执行 //sqliteDB.query(execstr, [], (err, rows) => { err ? (console.error(`exec error & sql:`, execstr, ` error:`, err, ` rows:`, curRows)) : null; });
-                                        sqlite3DB.exec(execstr); // console.log(`cur rows:`, JSON.stringify(curRows).slice(0, 100), ` page :`, page); //console.log(`statement execstr:`, execstr.slice(0, 100), ` exec success... page: `, page);
-                                    } catch (error) {
-                                        console.log(`sqlite db exec error:`, error);
-                                    } finally {
-                                        ++page;
-                                    }
+                        //对小于等于当前最大值的数据，进行检查并更新操作，异步
+                    } catch (error) {
+                        console.log(`increment sync error:`, error);
+                    }
+                } else { /***************** 方案二 全量 *****************/
+                    cache.setValue(cacheKey, `true`, 3600 * 24 * 365 * 1000);
+                    const querySQL = `select * from ${tableName} order by id desc `; //需要检查ID是否存在
+                    const qTableName = `${tableName}`;
+                    console.log(`exec #sync# tablename#${tableName}# >>>>>>>>>>>>>> :`, keys, ` select sql :`, querySQL);
+                    try {
+                        //查询主数据库所有数据，全部插入本地数据库中
+                        pool.query(querySQL, [], (error, rows, _fields) => {
+                            try {
+                                if (error) { //如果执行错误，则直接返回
+                                    return console.log("mysql sync to sqlite >>>>> ", error);
                                 }
-                                sqlite3DB.exec('COMMIT');
-                                console.log(`database> sync tablename:`, qTableName, ` over ... `);
-                            })();
-                        } catch (error) {
-                            console.log(`sql error:`, error);
-                        }
-                        return true;
-                    });
-                } catch (error) {
-                    console.log(`full scale sync error:`, error);
+                                (async() => {
+                                    console.log(`database> querySQL: ${querySQL} tablename:`, qTableName, ' rows length:', rows.length);
+                                    const pageSize = 1; // 10条批量执行
+                                    let page = 1,
+                                        maxRow = 0,
+                                        maxPage = Math.ceil(rows.length / pageSize);
+
+                                    sqlite3DB.exec('BEGIN TRANSACTION');
+                                    while (page <= maxPage) {
+                                        try {
+                                            startPage = pageSize * (page - 1);
+                                            maxRow = pageSize * (page - 0);
+                                            const curRows = rows.slice(startPage, maxRow);
+                                            const statement = tools.parseInsertStatement(qTableName, curRows, metaDB);
+                                            let execstr = sqlstring.format(statement.query, statement.params);
+                                            execstr = execstr.replace(/\r|\n/g, ''); //执行插入语句前，先查询数据库中是否存在此数据，若存在，则不执行 //sqliteDB.query(execstr, [], (err, rows) => { err ? (console.error(`exec error & sql:`, execstr, ` error:`, err, ` rows:`, curRows)) : null; });
+                                            sqlite3DB.exec(execstr); // console.log(`cur rows:`, JSON.stringify(curRows).slice(0, 100), ` page :`, page); //console.log(`statement execstr:`, execstr.slice(0, 100), ` exec success... page: `, page);
+                                        } catch (error) {
+                                            console.log(`sqlite db exec error:`, error);
+                                        } finally {
+                                            ++page;
+                                        }
+                                    }
+                                    sqlite3DB.exec('COMMIT');
+                                    console.log(`database> sync tablename:`, qTableName, ` over ... `);
+                                })();
+                            } catch (error) {
+                                console.log(`sql error:`, error);
+                            }
+                            return true;
+                        });
+                    } catch (error) {
+                        console.log(`full scale sync error:`, error);
+                    }
                 }
+                await tools.sleep(sync_interval_milisecond);
             }
-            await tools.sleep(sync_interval_milisecond);
-        }
-    })();
+        })();
+    });
 }
 
 /**
