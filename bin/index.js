bf601ef949d7aa6018d8dddbeb4ee5b0c7921149
yunwisdom <qq695926920@163.com>
2021-05-08 09:04:27 +0800

    init 新增sqlite初始化时，新增trycatch功能

--- bin/index.js
+++ bin/index.js
@@ -64,23 +64,31 @@ const initSqliteDB = async(pool = { query: () => {} }, metaDB = {}) => {
             const cacheKey = `init_sqlite_${tableName}_${ipaddress}_${version}`;
             const flag = await cache.getValue(cacheKey);
             let initSQL = cacheddl[tableName];
-            if (tools.isNull(initSQL) || initSQL == 'generate' || initSQL == 'auto') {
-                initSQL = await generateDDL(database, tableName, pool);
+            try {
+                if (tools.isNull(initSQL) || initSQL == 'generate' || initSQL == 'auto') {
+                    initSQL = await generateDDL(database, tableName, pool);
+                }
+            } catch (error) {
+                console.error(`generate ddl error:`, error);
             }
-            if (flag != `true`) { // await sqliteDB.query(initSQL); // memoryDB.query(initSQL);
-                sqliteDB.query(initSQL); 
-                sqlite3DB.exec('BEGIN TRANSACTION');
-                sqlite3DB.exec(initSQL);
-                sqlite3DB.exec('COMMIT');
-                cache.setValue(cacheKey, `true`, 3600 * 24 * 365 * 1000);
-                console.log(`cache key: ${cacheKey} flag: ${flag} init sql:`, initSQL);
-                console.info(`cache key: ${cacheKey} flag: ${flag} init sql:`, initSQL);
-                console.error(`cache key: ${cacheKey} flag: ${flag} init sql:`, initSQL);
-            } 
+            try {
+                if (flag != `true` && !tools.isNull(initSQL)) { // await sqliteDB.query(initSQL); // memoryDB.query(initSQL);
+                    sqliteDB.query(initSQL);
+                    sqlite3DB.exec('BEGIN TRANSACTION');
+                    sqlite3DB.exec(initSQL);
+                    sqlite3DB.exec('COMMIT');
+                    cache.setValue(cacheKey, `true`, 3600 * 24 * 365 * 1000);
+                    console.log(`cache key: ${cacheKey} flag: ${flag} init sql:`, initSQL);
+                    console.info(`cache key: ${cacheKey} flag: ${flag} init sql:`, initSQL);
+                    console.error(`cache key: ${cacheKey} flag: ${flag} init sql:`, initSQL);
+                }
+            } catch (error) {
+                console.error(`exec ddl error:`, error);
+            }
+            await tools.sleep(100);
         }
     })();
 
-    await tools.sleep(1500);
 }
 
 /**
@@ -109,57 +117,65 @@ const syncSqliteDB = async(pool = { query: () => {} }, metaDB = {}) => {
             const flag = await cache.getValue(cacheKey);
             console.log(`cache key: ${cacheKey} flag: ${flag} . `);
             if (flag == `true`) { /***************** 方案一 增量 *****************/
-                //查询本地sqlite数据，获取当前最大值 id , xid
+                try {
+                    //查询本地sqlite数据，获取当前最大值 id , xid
 
-                //查询主数据库数据库大于当前最大值的数据 id 新增 //将多的数据同步新增过来
+                    //查询主数据库数据库大于当前最大值的数据 id 新增 //将多的数据同步新增过来
 
-                //查询主数据库数据等于当前最大值得数据 xid 更新 //将多的数据同步更新过来
+                    //查询主数据库数据等于当前最大值得数据 xid 更新 //将多的数据同步更新过来
 
-                //对小于等于当前最大值的数据，进行检查并更新操作，异步
+                    //对小于等于当前最大值的数据，进行检查并更新操作，异步
+                } catch (error) {
+                    console.log(`increment sync error:`, error);
+                }
             } else { /***************** 方案二 全量 *****************/
                 cache.setValue(cacheKey, `true`, 3600 * 24 * 365 * 1000);
-                const querySQL = `select * from ${tableName} order by id desc `;
+                const querySQL = `select * from ${tableName} order by id desc `; //需要检查ID是否存在
                 const qTableName = `${tableName}`;
                 console.log(`exec #sync# tablename#${tableName}# >>>>>>>>>>>>>> :`, keys, ` select sql :`, querySQL);
-                //查询主数据库所有数据，全部插入本地数据库中
-                pool.query(querySQL, [], (error, rows, _fields) => {
-                    try {
-                        if (error) { //如果执行错误，则直接返回
-                            return console.log("mysql sync to sqlite >>>>> ", error);
-                        }
-                        (async() => {
-                            console.log(`database> querySQL: ${querySQL} tablename:`, qTableName, ' rows length:', rows.length);
-                            const pageSize = 1; // 10条批量执行
-                            let page = 1,
-                                maxRow = 0,
-                                maxPage = Math.ceil(rows.length / pageSize);
-
-                            sqlite3DB.exec('BEGIN TRANSACTION');
-                            while (page <= maxPage) {
-                                try {
-                                    startPage = pageSize * (page - 1);
-                                    maxRow = pageSize * (page - 0);
-                                    const curRows = rows.slice(startPage, maxRow);
-                                    const statement = tools.parseInsertStatement(qTableName, curRows, metaDB);
-                                    let execstr = sqlstring.format(statement.query, statement.params);
-                                    execstr = execstr.replace(/\r|\n/g, ''); //执行插入语句前，先查询数据库中是否存在此数据，若存在，则不执行 //sqliteDB.query(execstr, [], (err, rows) => { err ? (console.error(`exec error & sql:`, execstr, ` error:`, err, ` rows:`, curRows)) : null; });
-                                    sqlite3DB.exec(execstr); // console.log(`cur rows:`, JSON.stringify(curRows).slice(0, 100), ` page :`, page); //console.log(`statement execstr:`, execstr.slice(0, 100), ` exec success... page: `, page);
-                                } catch (error) {
-                                    console.log(`sqlite db exec error:`, error);
-                                } finally {
-                                    ++page;
-                                }
+                try {
+                    //查询主数据库所有数据，全部插入本地数据库中
+                    pool.query(querySQL, [], (error, rows, _fields) => {
+                        try {
+                            if (error) { //如果执行错误，则直接返回
+                                return console.log("mysql sync to sqlite >>>>> ", error);
                             }
-                            sqlite3DB.exec('COMMIT');
-                            console.log(`database> sync tablename:`, qTableName, ` over ... `);
-                        })();
-                    } catch (error) {
-                        console.log(`sql error:`, error);
-                    }
-                    return true;
-                });
+                            (async() => {
+                                console.log(`database> querySQL: ${querySQL} tablename:`, qTableName, ' rows length:', rows.length);
+                                const pageSize = 1; // 10条批量执行
+                                let page = 1,
+                                    maxRow = 0,
+                                    maxPage = Math.ceil(rows.length / pageSize);
+
+                                sqlite3DB.exec('BEGIN TRANSACTION');
+                                while (page <= maxPage) {
+                                    try {
+                                        startPage = pageSize * (page - 1);
+                                        maxRow = pageSize * (page - 0);
+                                        const curRows = rows.slice(startPage, maxRow);
+                                        const statement = tools.parseInsertStatement(qTableName, curRows, metaDB);
+                                        let execstr = sqlstring.format(statement.query, statement.params);
+                                        execstr = execstr.replace(/\r|\n/g, ''); //执行插入语句前，先查询数据库中是否存在此数据，若存在，则不执行 //sqliteDB.query(execstr, [], (err, rows) => { err ? (console.error(`exec error & sql:`, execstr, ` error:`, err, ` rows:`, curRows)) : null; });
+                                        sqlite3DB.exec(execstr); // console.log(`cur rows:`, JSON.stringify(curRows).slice(0, 100), ` page :`, page); //console.log(`statement execstr:`, execstr.slice(0, 100), ` exec success... page: `, page);
+                                    } catch (error) {
+                                        console.log(`sqlite db exec error:`, error);
+                                    } finally {
+                                        ++page;
+                                    }
+                                }
+                                sqlite3DB.exec('COMMIT');
+                                console.log(`database> sync tablename:`, qTableName, ` over ... `);
+                            })();
+                        } catch (error) {
+                            console.log(`sql error:`, error);
+                        }
+                        return true;
+                    });
+                } catch (error) {
+                    console.log(`full scale sync error:`, error);
+                }
             }
-            await tools.sleep(3000);
+            await tools.sleep(300);
         }
     })();
 }
@@ -169,41 +185,41 @@ const syncSqliteDB = async(pool = { query: () => {} }, metaDB = {}) => {
  */
 const generateDDL = async(database = 'xdata', tableName = '', pool = { query: () => {} }) => {
 
-    if (tools.isNull(tableName)) {
-        return '';
-    }
+        if (tools.isNull(tableName)) {
+            return '';
+        }
 
-    const cacheKey = `generate_sqlite_rows_flag`;
-    const flag = await cache.getValue(cacheKey);
-    const querySQL = "SELECT `c`.`table_name`, `c`.`column_name`, `c`.`ordinal_position`, `c`.`column_key`, `c`.`is_nullable`, `c`.`column_type`, `c`.`column_default` FROM ((`information_schema`.`columns` AS `c` LEFT JOIN `information_schema`.`key_column_usage` AS `k` ON `c`.`column_name` = `k`.`column_name` AND `c`.`table_schema` = `k`.`referenced_table_schema` AND `c`.`table_name` = `k`.`table_name`) LEFT JOIN `information_schema`.`statistics` AS `s` ON `c`.`column_name` = `s`.`column_name` AND `c`.`table_schema` = `s`.`index_schema` AND `c`.`table_name` = `s`.`table_name`) LEFT JOIN `information_schema`.`VIEWS` AS `v` ON `c`.`table_schema` = `v`.`table_schema` AND `c`.`table_name` = `v`.`table_name` WHERE `c`.`table_schema` = ':table_schema' AND `v`.`table_name` IS NULL ORDER BY `c`.`table_name`, `c`.`ordinal_position` ".replace(/:table_schema/g, database);
+        const cacheKey = `generate_sqlite_rows_flag`;
+        const flag = await cache.getValue(cacheKey);
+        const querySQL = "SELECT `c`.`table_name`, `c`.`column_name`, `c`.`ordinal_position`, `c`.`column_key`, `c`.`is_nullable`, `c`.`column_type`, `c`.`column_default` FROM ((`information_schema`.`columns` AS `c` LEFT JOIN `information_schema`.`key_column_usage` AS `k` ON `c`.`column_name` = `k`.`column_name` AND `c`.`table_schema` = `k`.`referenced_table_schema` AND `c`.`table_name` = `k`.`table_name`) LEFT JOIN `information_schema`.`statistics` AS `s` ON `c`.`column_name` = `s`.`column_name` AND `c`.`table_schema` = `s`.`index_schema` AND `c`.`table_name` = `s`.`table_name`) LEFT JOIN `information_schema`.`VIEWS` AS `v` ON `c`.`table_schema` = `v`.`table_schema` AND `c`.`table_name` = `v`.`table_name` WHERE `c`.`table_schema` = ':table_schema' AND `v`.`table_name` IS NULL ORDER BY `c`.`table_name`, `c`.`ordinal_position` ".replace(/:table_schema/g, database);
 
-    let ddlSQL = `CREATE TABLE IF NOT EXISTS ${tableName} ( `;
+        let ddlSQL = `CREATE TABLE IF NOT EXISTS ${tableName} ( `;
 
-    pool.query(querySQL, [], (error, rows, _fields) => {
-        cache.setValue(cacheKey, `true`, 3600 * 24 * 365 * 1000);
-        cache.setValue(cacheKey.replace('_flag', '_value'), JSON.stringify(rows), 3600 * 24 * 365 * 1000);
-    });
+        pool.query(querySQL, [], (error, rows, _fields) => {
+            cache.setValue(cacheKey, `true`, 3600 * 24 * 365 * 1000);
+            cache.setValue(cacheKey.replace('_flag', '_value'), JSON.stringify(rows), 3600 * 24 * 365 * 1000);
+        });
 
-    if (flag != 'true') { //查询表字段信息
-        await tools.sleep(15000);
-    }
+        if (flag != 'true') { //查询表字段信息
+            await tools.sleep(15000);
+        }
 
-    let rows = await cache.getValue(cacheKey.replace('_flag', '_value'));
-    rows = JSON.parse(rows);
+        let rows = await cache.getValue(cacheKey.replace('_flag', '_value'));
+        rows = JSON.parse(rows);
 
-    //筛选数据，选出表名称的数据
-    rows = rows.filter((item) => {
-        return item['table_name'] == tableName;
-    });
-    rows = rows.filter((item , index) => {
-        const _index = rows.findIndex(elem=>{return elem['column_name'] == item['column_name']})
-        return _index == index;
-    });
-    console.log(`generate create table ddl rows:`, rows);
-    console.error(`generate create table ddl rows:`, rows);
-    //根据表字段数据生成建表语句
-    for (const element of rows) {
-        ddlSQL += ` ${element['column_name']}  ${element['column_type']}  ${element['column_default'] == 'CURRENT_TIMESTAMP' ? ' default CURRENT_TIMESTAMP ': (element['column_type'].includes('char') && !tools.isNull(element['column_default']) ? ` default '${element['column_default']}' ` : (!tools.isNull(element['column_default']) ?  ` default ${element['column_default']} ` : '') ) }   ${element['is_nullable'] == 'YES' ? ' null ' : ' not null ' }  ${element['column_key'] == 'PRI' ? ' primary key ' : ''} \n ,`;
+        //筛选数据，选出表名称的数据
+        rows = rows.filter((item) => {
+            return item['table_name'] == tableName;
+        });
+        rows = rows.filter((item, index) => {
+            const _index = rows.findIndex(elem => { return elem['column_name'] == item['column_name'] })
+            return _index == index;
+        });
+        console.log(`generate create table ddl rows:`, rows);
+        console.error(`generate create table ddl rows:`, rows);
+        //根据表字段数据生成建表语句
+        for (const element of rows) {
+            ddlSQL += ` ${element['column_name']}  ${element['column_type']}  ${element['column_default'] == 'CURRENT_TIMESTAMP' ? ' default CURRENT_TIMESTAMP ': (element['column_type'].includes('char') && !tools.isNull(element['column_default']) ? ` default '${element['column_default']}' ` : (!tools.isNull(element['column_default']) ?  ` default ${element['column_default']} ` : '') ) }   ${element['is_nullable'] == 'YES' ? ' null ' : ' not null ' }  ${element['column_key'] == 'PRI' ? ' primary key ' : ''} \n ,`;
     }
     ddlSQL = ddlSQL.replace(/,$/gi,"");
     ddlSQL += ' ) '; // 建表语句封尾
