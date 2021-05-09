169438d883539616aa3708d1fbfabcac98d5ea2f
yunwisdom <qq695926920@163.com>
2021-05-09 15:01:27 +0800

    init

--- bin/config/config.js
+++ bin/config/config.js
@@ -95,7 +95,7 @@ module.exports = () => {
     };
 
     config.memorycache = {
-        version: 'v2.1.3-beta',
+        version: 'v2.1.4-beta',
         init_wait_milisecond: 100,
         sync_wait_milisecond: 5000,
         sync_interval_milisecond: 1000,
