fbc42168e3c2404e8dbdd128f31c84a04df3f89c
yunwisdom <qq695926920@163.com>
2021-05-12 08:58:52 +0800

    init

--- bin/index.js
+++ bin/index.js
@@ -119,10 +119,10 @@ const startXmysql = async(sqlConfig) => {
                 const metaDB = moreApis.getXSQL().getMetaDB();
                 databaseMap.set('meta_db_info', metaDB);
                 await sqlitetask.initSqliteDB(mysqlPool, metaDB, sqliteDBMap); //启动Sqlite本地缓存 进行两次建表初始化操作，避免写入操作时出现表不存在的异常
-                await tools.sleep((memorycacheConfig.sync_wait_milisecond || 3000) * 2); //等待Nms
+                await tools.sleep((memorycacheConfig.sync_wait_milisecond || 3000)); //等待Nms
                 await sqlitetask.syncSqliteDB(mysqlPool, metaDB, sqliteDBMap); //同步主数据库数据到sqlite
             })();
-            await tools.sleep((memorycacheConfig.sync_wait_milisecond || 3000) * 2); //等待Nms
+            await tools.sleep((memorycacheConfig.sync_wait_milisecond || 3000)); //等待Nms
         });
         const task = schedule.scheduleJob(schedule_task_time, function() {
             lock.lockExec(`app:start_sqlite_inc_schedule_db:${ipaddress}:${version}:lock`, async() => {
